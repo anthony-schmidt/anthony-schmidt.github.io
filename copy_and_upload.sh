9c037ade2bbc6cac17f7efa17b43e457207517b1
Your Name <you@example.com>
2022-01-06 11:33:29 -0600

    "Practice Script Test"

---
+++ copy_and_upload.sh
@@ -0,0 +1,7 @@
+﻿
+cp index.html anthony-schmidt.github.io
+LS=$(ls -I "*.sln" -I "*.config" -I "*.io")
+echo $LS
+cp -r $LS anthony-schmidt.github.io
+OUTPUT1=$(find . -maxdepth 1 -type f -not -name "*.sln" -not -name "*.config")
+OUTPUT2=$(find $OUTPUT2 -maxdepth 1 -type f -not -name "*.config")
---
+++ scripts/copy_and_upload.sh
@@ -0,0 +1,11 @@
+﻿
+LS=$(ls -I "*.sln" -I "*.config" -I "*.io")
+echo "Files/Folders Copied Over: "
+echo "$LS"
+cp -r $LS anthony-schmidt.github.io
+cd anthony-schmidt.github.io
+git add *
+echo "Upload Name: "
+read commit_name
+git commit -m "\"$commit_name\""
+git push -u
