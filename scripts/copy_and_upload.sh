19f2cdb1948d59db4d679296f62900508db31a38
Your Name <you@example.com>
2022-01-06 12:14:38 -0600

    "Script Test 3"

--- scripts/copy_and_upload.sh
+++ scripts/copy_and_upload.sh
@@ -1,11 +1,30 @@
 ﻿
+#Get Files
 LS=$(ls -I "*.sln" -I "*.config" -I "*.io")
-echo "Files/Folders Copied Over: "
+
+#Message
+echo "-Files/Folders To Be Copied Over: "
 echo "$LS"
+
+#Input Name
+echo "-Press 0 to only copy, or type a name to upload: "
+read commit_name
+
+#Copy files
 cp -r $LS anthony-schmidt.github.io
+
+#Check exit
+if [ $commit_name -eq 0 ] 
+then
+	exit 0
+fi
+
+#Add files to git
 cd anthony-schmidt.github.io
 git add *
-echo "Upload Name: "
-read commit_name
+echo " "
+git status
+
+#Commit and push
 git commit -m "\"$commit_name\""
 git push -u
---
+++ scripts/download_and_copy.sh
@@ -0,0 +1,24 @@
+﻿
+#Cd into github directory
+cd anthony-schmidt.github.io
+
+#Get user input
+echo "Press 0 to just pull, press 1 to pull and copy"
+read user_input
+
+#Pull from git
+git reset --hard HEAD
+git pull
+
+#Check exit
+if [ $user_input -eq 0 ] 
+then
+	exit 0
+fi
+#Get Files
+LS=$(ls -I "*.sln" -I "*.config" -I "*.io")
+
+#Copy Files Over
+echo "Files/Folders Copied Over: "
+echo "$LS"
+cp -r $LS ..
