de69aa19e6810c98ebf2dbca622401b8c0d1de31
Your Name <you@example.com>
2022-07-05 22:55:49 -0500

    "Fixed Image Error"

--- my_functions.js
+++ my_functions.js
@@ -1,10 +1,9 @@
-﻿function fixImage() {
+﻿/*function fixImage() {
 	var small_image = document.getElementById("id_headshot");
 	var big_image = document.getElementById("id_hidden_headshot");
 	big_image.src = "my-images/updated_headshot.jpg"
 
 	big_image.onload = function () {
-		console.log("hi")
 		small_image.src = big_image.src;
 	}
-}
+}*/
